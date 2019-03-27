fe3e8f56caeb637ccf0a718fc813920306c4924b
Miguel Reyes <reyes.miguel@gmail.com>
2019-03-27 16:40:14 -0400

    Disable/enable DOM elements on click.

--- public/js/chat.js
+++ public/js/chat.js
@@ -22,6 +22,13 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
+// Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFromInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', (message) => {
     console.log('message', message);
     var li = jQuery('<li></li>');
@@ -40,8 +47,11 @@ socket.on('newLocationMessage', (message) => {
     jQuery('#messages').append(li);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault(); //Prevent full page refresh.
+
+    //disable form to avoid double-clicks
+    $messageFormButton.setAttribute('disabled', 'disabled');
 
     const message = e.target.elements.message.value;
 
@@ -49,6 +59,10 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
         from: 'User',
         text: message
     }, (error) => {
+        $messageFormButton.removeAttribute('disabled'); //re-enable
+        $messageFromInput.value = '';  //Clear input field
+        $messageFromInput.focus();
+
         if (error) {
             return console.log(error);
         }
@@ -57,16 +71,21 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 });
 
 
-document.querySelector('#send-location').addEventListener('click', () => {
+
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled');
+
     navigator.geolocation.getCurrentPosition((position) => {
         console.log(position);
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, (err) => {
+            $sendLocationButton.removeAttribute('disabled');
             console.log('Location shared!');
         });
     }, () => {
